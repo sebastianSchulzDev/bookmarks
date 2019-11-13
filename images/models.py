bab6d6ece7dc39997747fc02734fac14ff7859af
Sebastian <sebastianschulz.dev@gmail.com>
2019-11-13 14:07:09 +0100

    Create Image Model to save the images to database

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -12,7 +13,14 @@ class Image(models.Model):
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField()
     image = models.ImageField(upload_to='images/%Y/%m/%d')
-    description = models.DateField(auto_now_add=True, db_index=True)
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True, db_index=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            super(Image, self).save(*args, **kwargs)
